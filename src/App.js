b9da3a423755674434bd41fceb5ef69b4380cb44
parimaldahekar <63495164+parimaldahekar@users.noreply.github.com>
2023-02-26 16:30:14 +0530

    update home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full min-h-screen bg-[#f8f4ef] font-Montserrat">
+    <div className="px-5 xl:px-32 container mx-auto">
+        <Home/>
+        {/* All the Routes are Defined here */}
+       
+      </div>
     </div>
   );
 }
