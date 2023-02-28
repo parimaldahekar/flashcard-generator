624d439fd2c2057c1760617a54e8f76fd25f1728
parimaldahekar <63495164+parimaldahekar@users.noreply.github.com>
2023-02-28 12:43:10 +0530

    added carousel

--- src/Pages/FlashcardDetails.js
+++ src/Pages/FlashcardDetails.js
@@ -8,6 +8,8 @@ import ReactToPrint from "react-to-print";
 import { useNavigate, useParams } from "react-router-dom";
 import ShareModal from "../Components/Share/ShareModal";
 import TabHands from "../Assets/hands-tab.jpg";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const handlePrint = () => {
   window.print();
@@ -22,28 +24,59 @@ const ShowAllCreatedCards = ({ theme }) => {
   const openModal = () => setIsOpen(true);
 
 
-  //will receive the data from the store
-  const cards = useSelector((state) => state.flashcard.flashcards);
-  // console.log(cards)
-  const [ourCard, setOurCard] = useState({});
-  const [displayCard, setDisplayCard] = useState({});
+const cards = useSelector((state) => state.flashcard.flashcards);
+const [ourCard, setOurCard] = useState({});
+const [displayCard, setDisplayCard] = useState({});
+const [cardIndex, setCardIndex] = useState(0); // add new state for card index
+const cardData = ourCard.cards || [];
 
-  //Function to set the card in middle of the screen which selected by the user from the left sticky menu
-  const showCard = (id) => {
-    const displaySingleCard = ourCard.cards.filter((a) => a.cardid === id);
-    setDisplayCard(displaySingleCard[0]);
-  };
+const showCard = (id) => {
+  const displaySingleCard = ourCard.cards.filter((a) => a.cardid === id);
+  setDisplayCard(displaySingleCard[0]);
+  setCardIndex(ourCard.cards.findIndex((card) => card.cardid === id)); // update index when showing new card
+};
 
-  useEffect(() => {
-    if (!groupId || !cards) return;
-    //Filter the card from the received data as per user selection
-    const temp = cards.filter((a) => a.card.groupid === groupId);
-    setOurCard(temp[0].card);
-  }, [groupId, cards]);
+const renderCard = (card) => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {card.cardimg ? (
+        <img
+          className="object-contain w-[22rem] xl:w-[20vw] h-[18rem] p-6"
+          src={card.cardimg}
+          alt={card.cardimg}
+        />
+      ) : (
+        <img
+          src={TabHands}
+          alt="cardimage"
+          className="object-contain w-[22rem] xl:w-[20vw] h-[18rem] p-6"
+        />
+      )}
+      <p
+        className={`w-full p-6 py-10 word-break: break-all text-${
+          theme === "dark" ? "white" : "slate-600"
+        }`}
+      >
+        {card.carddescription}{" "}
+      </p>
+    </div>
+  );
+};
 
-  useEffect(() => {
-    ourCard.cards && setDisplayCard(ourCard.cards[0]);
-  }, [ourCard]);
+
+
+useEffect(() => {
+  if (!groupId || !cards) return;
+  const temp = cards.filter((a) => a.card.groupid === groupId);
+  setOurCard(temp[0].card);
+}, [groupId, cards]);
+
+useEffect(() => {
+  if (ourCard.cards) {
+    setDisplayCard(ourCard.cards[0]);
+    setCardIndex(0); // initialize index when loading cards
+  }
+}, [ourCard]);
 
   return (
     <>
@@ -78,6 +111,7 @@ const ShowAllCreatedCards = ({ theme }) => {
             <h1 className="p-3">Flashcards</h1>
             <hr />
             <hr className="mb-2" />
+
             {/* CREATING SIDE MENU */}
             {ourCard.cards &&
               ourCard.cards.map((card) => (
@@ -97,44 +131,78 @@ const ShowAllCreatedCards = ({ theme }) => {
             <div className="carousel_control">
             </div>
           </aside>
+
           {/*CARD IMAGE & CARD DESCRIPTION SECTION */}
           <section
             ref={componentRef}
             className={`col-span-3 md:col-span-2 md:m-1 flex flex-col xl:flex-row sm:my-5 items-center w-full bg-${theme === "dark" ? "dark" : "white"
               }  shadow-lg rounded-lg md:flex-col border-2`}
           >
-
-            {displayCard.cardimg ? (
-              <img
-                className="object-contain w-[32rem] xl:w-[20vw] h-full p-6"
-                src={displayCard.cardimg}
-                alt={displayCard.cardimg}
-              />
-            ) : (
-              <img
-                src={TabHands}
-                alt="cardimage"
-                className="object-contain w-[32rem] xl:w-[20vw] h-full p-6"
-              />
-            )}
-            <p
-              className={`w-full p-6 py-10 	word-break: break-all text-${theme === "dark" ? "white" : "slate-600"
-                }`}
-            >
-              {displayCard.carddescription}{" "}
-            </p>
+          <Carousel
+          additionalTransfrom={0}
+          arrows
+          autoPlaySpeed={3000}
+          centerMode={false}
+          className=""
+          containerClass="container mx-auto"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024,
+              },
+              items: 1,
+              partialVisibilityGutter: 40,
+            },
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0,
+              },
+              items: 1,
+              partialVisibilityGutter: 30,
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464,
+              },
+              items: 1,
+              partialVisibilityGutter: 30,
+            },
+          }}
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+          {cardData.map((card) => (
+            <div key={card.cardid}>{renderCard(card)}</div>
+          ))}
+        </Carousel>
+        
+           
           </section>
           {/*BUTTONS PART- SHARE, DOWNLOAD,PRINT */}
           <aside className="col-span-1 md:flex flex-col items-center ">
- 
-          <button
-          type="button"
-          onClick={openModal}
-          className={`flex items-center w-[60vw] md:w-[10rem] xl:w-[15rem] md:m-5 mr-5 px-4 py-4 h-fit rounded-md border-2 sm:w-[83vw] sm:mx-5 hover:scale-105  mt-2 py-3 px-4 xl:w-60 space-x-5  text-${theme === "dark" ? "white" : "slate-600"
-        } rounded-md shadow-lg  transition-all duration-100 hover:scale-105 border-2 sm:border-none bg-${theme === "dark" ? "dark" : "white"}`}        >
-          <RiArrowGoBackLine className="scale-x-[-1]" />
-          <span className="hidden xl:block">Share</span>
-        </button>
+
+            <button
+              type="button"
+              onClick={openModal}
+              className={`flex items-center w-[60vw] md:w-[10rem] xl:w-[15rem] md:m-5 mr-5 px-4 py-4 h-fit rounded-md border-2 sm:w-[83vw] sm:mx-5 hover:scale-105  mt-2 py-3 px-4 xl:w-60 space-x-5  text-${theme === "dark" ? "white" : "slate-600"
+                } rounded-md shadow-lg  transition-all duration-100 hover:scale-105 border-2 sm:border-none bg-${theme === "dark" ? "dark" : "white"}`}        >
+              <RiArrowGoBackLine className="scale-x-[-1]" />
+              <span className="hidden xl:block">Share</span>
+            </button>
 
             <ShareModal theme={theme} /> {/*IMPORTED SHARE COMPONENT */}
             {/* DOWNLOAD BUTTON*/}
@@ -165,6 +233,7 @@ const ShowAllCreatedCards = ({ theme }) => {
         <ShareModal isOpen={isOpen} closeModal={closeModal} />
 
       </section>
+    
     </>
   );
 };
